643d79ac0d64e926859cc63b26d585914cb186fd
Craig <3979063+craig8@users.noreply.github.com>
2023-07-07 17:50:46 +0000

    build and install topology_processor

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,10 @@ curl -sSL https://install.python-poetry.org | python3 -
 git clone https://github.com/GRIDAPPSD/topology-processor -b dsa
 cd topology_processor
 poetry install
+poetry build
+
+pip install dist/*.whl
+cd ../cimgraph-demo
+
 
 
