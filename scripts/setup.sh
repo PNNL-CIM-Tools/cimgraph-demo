80d2a80db80116f184182824804e711fb9875d11
Craig <3979063+craig8@users.noreply.github.com>
2023-07-07 10:42:13 -0700

    Initial setup for dev container

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+# Setup script will run post creation of the environment.
+curl -sSL https://install.python-poetry.org | python3 -
+
+git clone https://github.com/GRIDAPPSD/topology-processor -b dsa
+cd topology_processor
+poetry install
+
+
