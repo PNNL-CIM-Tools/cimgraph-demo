9b82d00a467b5d048acb498adc43ca31083ffa97
Alexander Anderson <alexander.a.anderson@ieee.org>
2023-07-10 13:57:19 -0700

    Install CIMHub and CIM-Graph

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,29 @@
 # Setup script will run post creation of the environment.
 curl -sSL https://install.python-poetry.org | python3 -
-
+# Install GridAPPS-D Topology Processor
 git clone https://github.com/GRIDAPPSD/topology-processor -b dsa
 cd topology_processor
 poetry install
 poetry build
-
 pip install dist/*.whl
-cd ../cimgraph-demo
+# Install CIMHub
+cd ..
+apt-get install maven
+git clone https://github.com/GRIDAPPSD/CIMHub.git -b final9500
+python3 -m pip install -e CIMHub
+cd CIMHub/cimhub
+mvn clean install
+cd ../..
+# Install CIMantic Graphs
+git clone https://github.com/PNNL-CIM-Tools/CIM-Graph.git -b techfest_2023
+cd cimgraph-demo
+poety install
+poetry build
+pip install dist/*.whl
+# Install DSS-Python
+pip install dss-python
 
+docker pull gridappsd/blazegraph
+docker run -d gridappsd/blazegraph
 
 
